7659e1cd665d2efa7626dda98ae043156e4ef9ed
adajass <jass.ada@qq.com>
2018-03-15 16:09:41 +0900

    Dkjg

--- mongoconnet.py
+++ mongoconnet.py
@@ -1,6 +1,6 @@
 import pymongo
 from pymongo import MongoClient
-__client = MongoClient('mongodb://admin:%2Bbeijing2017@node0:27017')
+__client = MongoClient('mongodb://admin:%2B@node0:27017')
 security =__client.quantDay.security
 finance = __client.quantDay.finance
 future = __client.quantDay.future
