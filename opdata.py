5abd51c798a8b5a343a4a1c27d4fea212f788973
Jass Ada <Jass Ada>
2018-07-19 18:20:44 +0800

    dkjg

--- opdata.py
+++ opdata.py
@@ -10,10 +10,10 @@ import os
 from tqdm import tqdm
 import talib
 
-from opdata import factors as _factors
-from opdata.mongoconnect import *  
-# import factors as _factors  
-# from mongoconnect import *
+# from opdata import factors as _factors
+# from opdata.mongoconnect import *  
+import factors as _factors  
+from mongoconnect import *
 
 __T = pd.read_csv(os.path.join(os.path.dirname(os.path.realpath(__file__)),'calAll.csv'))
 __TM = ts.get_k_data('000001', ktype='M', index=True)[['date']]
@@ -211,6 +211,15 @@ def _fetch_finance():
             rep=rep.merge(cas,on='code',how='left')
             finance.insert(rep.to_dict('record'))
             print(year, quarter)
+
+def _fetch_forecast():
+    for year in range(2004,2018):
+        for quarter in range(1,5): 
+            print(year,' year ','quarter ',quarter)  
+            fcast = ts.forecast_data(year,quarter)
+            fcast.rename(columns={'report_date':'date'}, inplace=True)  
+            forecast.insert(fcast.to_dict('record'))          
+            # rep['date']=rep['date'].apply(set_year)
 
 def get_finance(code, start_date='2004-04-01', end_date='2017-10-10'):
     lastvalue = 0.0
@@ -429,6 +438,97 @@ def get_ts_finance(code, period):
         return df[df.index%128==0]
     pass
 
+def get_forecast(code, start_date='2004-01-01', end_date='2017-12-12'):
+    lastvalue = 0.0
+    def setValue(v):
+        nonlocal lastvalue
+        if pd.isnull(v) or v == 'None' or v == '--':
+            return lastvalue
+        else:
+            lastvalue = v
+            return lastvalue
+    cursor = forecast.find({'code':str(code), 'date':{'$gte':'2004-01-01', '$lte': end_date}}).sort('date')
+    df = pd.DataFrame(list(cursor))
+    if df.empty:
+        return df
+    del df['_id']
+    del df['code']
+    T = __T    
+    T.rename(columns={'calendarDate':'date'}, inplace=True)
+    # T = T[T.date > '2003-01-01']
+    T=T.merge(df,on='date',how='left')
+    T=T.drop_duplicates(['date'])
+    for column in T:
+        if column != 'code' and column != 'date':
+            try:
+                T[[column]] = T[[column]].astype(float)
+            except ValueError:
+                pass            
+            lastvalue = 0.0
+            T[column]=T[column].apply(setValue)
+            
+    T = T[T.isOpen >0.5]
+    T = T[T.date >= start_date]
+    T = T[T.date <= end_date]
+    T['code']=str(code)
+    T = T.reset_index()
+    del T['index']
+    del T['isOpen']
+    return T
+
+def get_holdfund(code, start_date='2004-01-01', end_date='2017-12-12'):
+    lastvalue = 0.0
+    def setValue(v):
+        nonlocal lastvalue
+        if pd.isnull(v) or v == 'None' or v == '--':
+            return lastvalue
+        else:
+            lastvalue = v
+            return lastvalue
+    cursor = holdfund.find({'code':str(code), 'date':{'$gte':'2004-01-01', '$lte': end_date}}).sort('date')
+    hfdf = pd.DataFrame(list(cursor))
+    if hfdf.empty:
+        return hfdf
+    df = pd.DataFrame(columns=['date','fund_holders','t_shares','t_share_rate','t_market_value','t_net_rate'])
+    currentdate=''    
+    fund_holders,t_shares,t_share_rate,t_market_value,t_net_rate = 0,0,0.0,0.0,0.0
+    for col in range(len(hfdf)):
+        if hfdf.iloc[col].date != currentdate:
+            if fund_holders > 0:
+                df.loc[len(df)] = [currentdate,fund_holders,t_shares,t_share_rate,t_market_value,t_net_rate]
+            currentdate = hfdf.iloc[col].date
+            fund_holders,t_shares,t_share_rate,t_market_value,t_net_rate = 0.0,0.0,0.0,0.0,0.0
+        else:
+            fund_holders +=1
+            t_shares += float(hfdf.iloc[col].hold_shares)
+            t_share_rate += float(hfdf.iloc[col].liquid_share_rate)
+            t_market_value += float(hfdf.iloc[col].maket_value)
+            t_net_rate += float(hfdf.iloc[col].net_value_rate)
+
+    
+    T = __T    
+    T.rename(columns={'calendarDate':'date'}, inplace=True)
+    # T = T[T.date > '2003-01-01']
+    T=T.merge(df,on='date',how='left')
+    T=T.drop_duplicates(['date'])
+    for column in T:
+        if column != 'code' and column != 'date':
+            try:
+                T[[column]] = T[[column]].astype(float)
+            except ValueError:
+                pass            
+            lastvalue = 0.0
+            T[column]=T[column].apply(setValue)
+            
+    T = T[T.isOpen >0.5]
+    T = T[T.date >= start_date]
+    T = T[T.date <= end_date]
+    T['code']=str(code)
+    T = T.reset_index()
+    del T['index']
+    del T['isOpen']
+    return T
+
 def __make_period__(period, start_date, end_date):
     T = __T
     T.rename(columns={'calendarDate':'date'}, inplace=True)    
@@ -569,7 +669,8 @@ def get_all(pool, period, start_date, factors=[], count=0, index=True, **args):
         'rateofreturn', 'seg', 'cf_liabilities', 'cf_nm', 'arturndays', 'epcf', 'code', \
         'mbrg', 'nprg', 'business_income', 'EBT2TA', 'cf_sales', 'eps', 'arturnover', \
         'net_profit_ratio', 'EBITDA2TA', 'quickratio', 'bips', 'low', 'EBITDA', 'EBIT', \
-        'general_equity', 'flow_equity', 'EBITDA2', 'pe']
+        'general_equity', 'flow_equity', 'EBITDA2', 'pe','fund_holders','t_shares',\
+        't_share_rate','t_market_value','t_net_rate', 'pre_eps', 'range', 'type']
     code_list = list(dfM[thedate])
     # print(code_list)
     # exit()
@@ -585,6 +686,8 @@ def get_all(pool, period, start_date, factors=[], count=0, index=True, **args):
             df_price['name'] = 'sh300'
         df_finance = get_finance(code, '1995-01-01', end_date)
         jp_finance = _factors.JP_VALUATION_FINANCE(code,'1995-01-01',end_date)
+        df_holdfund = get_holdfund(code,'2004-01-01',end_date)
+        df_forecast = get_forecast(code,'2004-01-01',end_date)
         # print(jp_finance)
         # exit()
         #merge
@@ -600,6 +703,12 @@ def get_all(pool, period, start_date, factors=[], count=0, index=True, **args):
             df = df.merge(jp_finance, how='left', on ='date', suffixes=('', '_y'))
             drop_y(df)
             # print(df[df.date>'2008-01-08'])
+        if not df_holdfund.empty:
+            df = df.merge(df_holdfund, how='left', on ='date', suffixes=('', '_y'))
+            drop_y(df)
+        if not df_forecast.empty:
+            df = df.merge(df_forecast, how='left', on ='date', suffixes=('', '_y'))
+            drop_y(df)
         
         t_columns=list(df.columns)
         for it in all_columns:
@@ -798,10 +907,13 @@ if __name__ == '__main__':
     # _fetch_finance()
     # print(get_finance('000001'))
     # print(get_local_future('A99'))
+    print(get_forecast('000001'))
+    # print(get_holdfund('000001'))
     # print(get_future('XAU/USD'))
     # print(get_month('2010-01'))
     # print(get_ts_finance('000001','1m'))
-    re = get_all('test','1m','2010-01', ['open', 'vol_1_1m','rsi_10_1m', 'EBITDA2TA'],index=False)
-    print(re)
+    # re = get_all('test','1m','2010-01', ['open', 'vol_1_1m','rsi_10_1m', 'EBITDA2TA'],index=False)
+    # print(re)
+    # _fetch_forecast()
     # print(re[1])
     # print(re[2])    
